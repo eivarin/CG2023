52f749c22ebaedd3eafe6e20f47d8257ac254711
acmota2 <acmota2@gmail.com>
2023-05-05 15:03:26 +0100

    A órbita está errada, mas já vejo isso

--- test_files_custom/orbitas.py
+++ test_files_custom/orbitas.py
@@ -7,7 +7,7 @@ lowest = int(input("Inserir altura mínima: "))
 
 highest_x_z = average + dif
 lowest_x_z = average - dif
-new_average = (average**2 / 2)**0.5
+new_average = ((average**2) / 2)**0.5
 
 height_mid = (highest + lowest) / 2
 height_high_mid = (highest + height_mid) / 2
@@ -18,7 +18,7 @@ print(f"""<translate time="{time}" align="true" segments="10000">
     <point x="{new_average}" y="{height_high_mid}" z="{new_average}" />
     <point x="0" y="{height_mid}" z="{lowest_x_z}" />
     <point x="{-new_average}" y="{height_low_mid}" z="{new_average}" />
-    <point x="{-highest_x_z}" y="{height_mid}" z="0" />
+    <point x="{-highest_x_z}" y="{lowest}" z="0" />
     <point x="{-new_average}" y="{height_low_mid}" z="{-new_average}" />
     <point x="0" y="{height_mid}" z="{-lowest_x_z}" />
     <point x="{new_average}" y="{height_high_mid}" z="{-new_average}" />
